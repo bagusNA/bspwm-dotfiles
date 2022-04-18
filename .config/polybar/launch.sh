7c6ab085a34ba2d5f596e48627f4bcb9cccfbfd5
bagusnur <bagusnur@protonmail.com>
2022-04-18 09:10:34 +0800

    cleanup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
 # Terminate already running bar instances
 killall -q polybar
-# polybar-msg cmd restart
 
 ## Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
